495e309bdae2a6763877a032df974ca70cffd728
srmacpherson <srmacpherson96@gmail.com>
2025-11-24 12:10:47 +0000

    create users collection schema and add GET and POST endpoints to the database using mongoose.

--- app.js
+++ app.js
@@ -1,11 +1,21 @@
 const express = require("express");
 const app = express();
 const mongoose = require("mongoose");
+const usersRouter = require('./routes/usersRouter')
 
 require("dotenv").config();
 
 const cors = require("cors");
 app.use(cors());
+
+mongoose.connect(process.env.DATABASE_URL);
+const db = mongoose.connection;
+db.on('error', (err) => {
+    console.error(err);
+})
+db.once('open', () => {
+    console.log('connected to database');
+})
 
 // console.log("env ", process.env.DATABASE_URL);
 
@@ -21,5 +31,7 @@ app.use(cors());
 // });
 
 app.use(express.json());
+
+app.use('/users', usersRouter);
 
 module.exports = app;
---
+++ routes/usersRouter.js
@@ -0,0 +1,31 @@
+const express = require("express");
+const router = express.Router();
+const User = require("../models/user");
+
+router.get("/", async (req, res) => {
+  try {
+    const users = await User.find();
+    res.json(users);
+  } catch (err) {
+    res.status(500).json({message: err.message})
+  }
+});
+
+router.post('/', async (req, res) => {
+    const user = new User({
+        name: req.body.name,
+        username: req.body.username,
+        age: req.body.age,
+    });
+
+    try {
+        const newUser = await user.save();
+        res.status(201).json(newUser);
+    }
+        catch (err) {
+            res.status(400).json({message: err.message})
+        }
+    }
+)
+
+module.exports = router;
