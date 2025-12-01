04698d7427d469ca2ba41ad95e98562cf0ed1ae8
APigott1 <pigster99@gmail.com>
2025-12-01 10:40:12 +0000

    add divide by 0 protection

--- utils/computeCompatibility.js
+++ utils/computeCompatibility.js
@@ -2,6 +2,7 @@ function computeCompatibility(selfGenres, userGenres) {
   const selfSet = new Set(selfGenres);
   const userSet = new Set(userGenres);
 
+  if (selfSet.union(userSet).size === 0) return 0;
   return selfSet.intersection(userSet).size / selfSet.union(userSet).size;
 }
 
