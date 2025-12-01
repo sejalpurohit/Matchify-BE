8167fbb4136b3d7e09a46f92198d3fff44c076b1
APigott1 <pigster99@gmail.com>
2025-12-01 10:41:01 +0000

    add patch matches endpoint for like / dislike buttons

--- routes/usersRouter.js
+++ routes/usersRouter.js
@@ -23,13 +23,45 @@ router.put("/feed", async (req, res) => {
       return userObj;
     });
 
-    const filteredUsers = users.filter(
-      (user) => user.spotifyId !== userSpotifyId
-    );
+    const filteredUsers = users.filter((user) => {
+      return (
+        user.spotifyId !== userSpotifyId &&
+        !user.matches.includes(userSpotifyId) &&
+        !user.liked.includes(userSpotifyId) &&
+        !user.passed.includes(userSpotifyId)
+      );
+    });
 
     filteredUsers.sort((a, b) => b.compatibility - a.compatibility);
 
     res.json(filteredUsers);
+  } catch (err) {
+    res.status(500).json({ message: err.message });
+  }
+});
+
+router.patch("/matches", async (req, res) => {
+  const userId = req.body.spotifyId;
+  const otherId = req.body.otherSpotifyId;
+  const isLike = req.body.isLike;
+
+  try {
+    const user = await User.findOne({ spotifyId: userId });
+    const other = await User.findOne({ spotifyId: otherId });
+    if (isLike) {
+      if (other.liked.includes(user.spotifyId)) {
+        user.matches.push(other.spotifyId);
+        other.matches.push(user.spotifyId);
+        other.liked = other.liked.filter((id) => id != user.spotifyId);
+      } else {
+        user.liked.push(other.spotifyId);
+      }
+    } else if (isLike === false) {
+      user.passed.push(other.spotifyId);
+    }
+    const updatedUser = await user.save();
+    const updatedOther = await other.save();
+    res.json({ updatedUser, updatedOther });
   } catch (err) {
     res.status(500).json({ message: err.message });
   }
@@ -74,5 +106,10 @@ router.post("/", async (req, res) => {
     res.status(400).json({ message: err.message });
   }
 });
+
+// router.patch("/", async (req, res) => {
+//   const userData = req.body.user;
+
+// })
 
 module.exports = router;
